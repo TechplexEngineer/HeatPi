e8ffdd5102e39d52daeb9d36493b383dbdf76647
Blake Bourque <techplex.engineer@gmail.com>
2024-12-01 07:27:57 -0500

    Add code to setup and advertise hostname

--- app/src/lib/startup.ts
+++ app/src/lib/startup.ts
@@ -2,6 +2,9 @@ import os from 'os';
 import { RelayBox } from './relaybox';
 import { TempMgr } from './tempmgr';
 import { ZoneMgr } from './zonemgr';
+import { setupHostname } from './utils/hostname';
+import { dev } from '$app/environment';
+
 const isRaspberryPi = os.arch() === 'arm64' && os.platform() === 'linux';
 
 export const startup = async (): Promise<App.Locals> => {
@@ -9,34 +12,15 @@ export const startup = async (): Promise<App.Locals> => {
 
     const i2cBus = isRaspberryPi ? await import('i2c-bus') : await import('$lib/utils/i2c-bus-mock');
 
-    const i2cBusNum = 1;
+    const i2cBusNum = parseInt(process.env.HEATPI_I2C_BUS_NUM ?? '1', 10);
     const con = await i2cBus.openPromisified(i2cBusNum);
 
     const relayBox = new RelayBox(con);
-
-    const hostname = "heatpi";
-
-    if (process.env.BALENA_SUPERVISOR_ADDRESS) {
-        console.log(`Updating hostname to: ${hostname}`);
-        const response = await fetch(`${process.env.BALENA_SUPERVISOR_ADDRESS}/v1/device/host-config?apikey=${process.env.BALENA_SUPERVISOR_API_KEY}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                network: {
-                    hostname: hostname
-                }
-            })
-        });
-
-        if (!response.ok) {
-            console.log(`Failed to update hostname: ${response.statusText}`);
-        } else {
-            console.log(`Hostname updated to ${hostname}`);
-        }
+    
+    if (!dev) {
+        setupHostname(process.env.HEATPI_HOSTNAME ?? "heatpi");
     } else {
-        console.log("Error: Skipping hostname change. Unable to access supervisor environment variables.");
+        console.log('Skipping hostname setup in dev mode');
     }
 
     return {
@@ -44,3 +28,4 @@ export const startup = async (): Promise<App.Locals> => {
         tempMgr: new TempMgr(con),
     };
 }
+
---
+++ app/src/lib/utils/hostname.ts
@@ -0,0 +1,41 @@
+import dbus from 'dbus-next';
+
+export const setupHostname = async (hostname: string) => {
+    setHostname(hostname);
+
+    advertiseHostname(hostname);
+};
+
+export const setHostname = async (hostname: string) => {
+    if (process.env.BALENA_SUPERVISOR_ADDRESS) {
+        console.log(`Updating hostname to: ${hostname}`);
+        const response = await fetch(`${process.env.BALENA_SUPERVISOR_ADDRESS}/v1/device/host-config?apikey=${process.env.BALENA_SUPERVISOR_API_KEY}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                network: {
+                    hostname: hostname
+                }
+            })
+        });
+
+        if (!response.ok) {
+            console.log(`Failed to update hostname: ${response.statusText}`);
+        } else {
+            console.log(`Hostname updated to ${hostname}`);
+        }
+    } else {
+        console.log("\x1b[31mError: Skipping hostname change. Unable to access supervisor environment variables.\x1b[0m");
+    }
+}
+
+export const advertiseHostname = async (hostname: string) => {
+    const bus = dbus.systemBus();
+    const obj = await bus.getProxyObject('org.freedesktop.Avahi', '/');
+    const server = obj.getInterface('org.freedesktop.Avahi.Server');
+
+    await server.SetHostName(hostname);
+    console.log(`Hostname advertised via Avahi: ${hostname}`);
+}

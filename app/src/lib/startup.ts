96972313da0f951adc5ecec0df2a2d234f632fe6
Blake Bourque <techplex.engineer@gmail.com>
2024-11-30 22:28:45 -0500

    Add code to change hostname on app start

--- app/src/lib/startup.ts
+++ app/src/lib/startup.ts
@@ -12,7 +12,32 @@ export const startup = async (): Promise<App.Locals> => {
     const i2cBusNum = 1;
     const con = await i2cBus.openPromisified(i2cBusNum);
 
-    const relayBox = new RelayBox(con)
+    const relayBox = new RelayBox(con);
+
+    const hostname = "heatpi";
+
+    if (process.env.BALENA_SUPERVISOR_ADDRESS) {
+        console.log(`Updating hostname to: ${hostname}`);
+        const response = await fetch(`${process.env.BALENA_SUPERVISOR_ADDRESS}/v1/device/host-config?apikey=${process.env.BALENA_SUPERVISOR_API_KEY}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                network: {
+                    hostname: hostname
+                }
+            })
+        });
+
+        if (!response.ok) {
+            console.log(`Failed to update hostname: ${response.statusText}`);
+        } else {
+            console.log(`Hostname updated to ${hostname}`);
+        }
+    } else {
+        console.log("Error: Skipping hostname change. Unable to access supervisor environment variables.");
+    }
 
     return {
         zoneMgr: new ZoneMgr(relayBox),

03d22f550235adca69dd443acfa0ce04147dc0b4
Blake Bourque <techplex.engineer@gmail.com>
2024-12-03 06:05:24 -0500

    Add bus scanning page

--- app/src/routes/bus/+page.server.ts
+++ app/src/routes/bus/+page.server.ts
@@ -1,11 +1,19 @@
 import type { Actions, PageServerLoad } from './$types';
 
 export const load = (async ({locals}) => {
-    const result = await locals.rawBus.scan()
-    console.log(`I2C Bus scan result:`, result);
-    return {};
+    // const result = await locals.rawBus.scan()
+    // console.log(`I2C Bus scan result:`, result);
+    return {
+        // deviceAddresses: result
+    };
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
-    
+    scan: async ({locals}) => {
+        const result = await locals.rawBus.scan()
+        console.log(`I2C Bus scan result:`, result);
+        return {
+            deviceAddresses: result
+        };
+    }
 };
--- app/src/routes/bus/+page.svelte
+++ app/src/routes/bus/+page.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
+    import type { PageData, ActionData } from './$types';
 
-    let { data }: { data: PageData } = $props();
+    let { data, form }: { data: PageData, form: ActionData } = $props();
 </script>
 
-<div class="container">
-    <h1>I2C Bus</h1>
+<form class="container" method="POST" action="?/scan" use:enhance>
+    <div class="d-flex justify-content-between">
+        <h1>I2C Bus</h1>
+        <div>
+            <button class="btn btn-primary mt-2">Scan</button>
+        </div>
+    </div>
     
-</div>
+    <ul>
+        {#each form?.deviceAddresses || [] as address}
+            <li>{address}</li>
+        {:else}
+            <li>No devices found</li>
+        {/each}
+    </ul>
+    
+
+</form>
